a9f223358395fa8ddbc2b9069a090b48a1b00351
Pranavv213 <quantumworld394@gmail.com>
2025-06-10 17:30:37 +0530

    ui modified

--- src/Components/Token.js
+++ src/Components/Token.js
@@ -523,7 +523,7 @@ export default function DeployConnectVerse() {
       setStatus("Waiting for transaction to be mined...");
       await contract.deployed();
 
-	  await addDoc(usersCollectionRef, {Name: tokenName, Symbol:tokenSymbol, Onwer:userAddress,Address:contract.address});
+	  await addDoc(usersCollectionRef, {Name: tokenName, Symbol:tokenSymbol, Onwer:userAddress,Address:contract.address,Email:localStorage.getItem('email')});
 
       setContractAddress(contract.address);
       setStatus("Contract deployed successfully!");
